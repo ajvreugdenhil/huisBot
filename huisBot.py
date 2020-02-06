2ee4707601a417d71fb0dcbb54eed1775a6fdbc8
Arjan <midborghmorgenstern@gmail.com>
2020-02-06 18:59:51 +0100

    fixed timer mechanism

--- huisBot.py
+++ huisBot.py
@@ -5,7 +5,7 @@ import logging
 from telegram.ext import Updater, CommandHandler
 from telegram import Bot
 import json
-from threading import Timer
+from threading import Timer, Thread, Event
 import time
 import datetime
 import dateutil.parser
@@ -15,19 +15,29 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-
 def getToken():
     file = open(userFileLocation + "token.txt", "r")
     token = file.read()
     return token
 
-userFileLocation = "./userData/"
-tasks = []
-bot = Bot(getToken())
-updater = Updater(bot=bot, use_context=True)
-started = False
+# Classes
 
-# Task code
+class perpetualTimer():
+   def __init__(self,t,method):
+      self.t=t
+      self.method = method
+      self.thread = Timer(self.t,self.handle_function)
+
+   def handle_function(self):
+      self.method()
+      self.thread = Timer(self.t,self.handle_function)
+      self.thread.start()
+
+   def start(self):
+      self.thread.start()
+
+   def cancel(self):
+      self.thread.cancel()
 
 
 class task:
@@ -49,15 +59,6 @@ class task:
         timeDifference = (self.startDate - currentDate).total_seconds()
         return timeDifference
 
-    def setTimer(self):
-        if (self.getSecondsToGo() > 0):
-            self.timer = Timer(self.getSecondsToGo(),
-                               notify, kwargs={'task': self})
-            self.timer.start()
-            self.active = True
-        else:
-            self.active = False
-
     def toString(self):
         return self.assignee +\
             " will do " + self.taskName +\
@@ -65,6 +66,15 @@ class task:
             "/" + str(self.startDate.month) +\
             "/" + str(self.startDate.day)
 
+
+# Settings
+# folder for the users' data
+userFileLocation = "./userData/"
+# check active tasks every x seconds
+updateSpeed = 5
+
+
+# Task importation code
 
 def getTasksFromFile(filename):
     file = open(userFileLocation + filename, "r")
@@ -83,54 +93,60 @@ def startTasksFromFile(chat_id, filename="tasks.json"):
                              newtask["startDate"],
                              newtaskdata["startMessage"])
         if (taskToBeBuilt.getSecondsToGo() > 0):
-            taskToBeBuilt.setTimer()
             tasks.append(taskToBeBuilt)
-
+    
 
 # Handlers
 
-def notify(task):
+def handleActiveTasks():
     global tasks
-    bot.send_message(chat_id=task.chat_id, text=task.getStartMessage())
-    taskIndex = tasks.index(task)
-    del tasks[taskIndex]
+    for task in tasks:
+        if (task.getSecondsToGo() <= 0):
+            bot.send_message(chat_id=task.chat_id, text=task.getStartMessage())
+            taskIndex = tasks.index(task)
+            del tasks[taskIndex]
 
 def welcome(update, context):
     file = open(userFileLocation + "welcome.txt", "r")
     welcomeText = file.read()
-    update.message.reply_text(welcomeText)
+    update.message.reply_text(welcomeText, parse_mode="Markdown")
 
+
+def debug(update, context):
+    pass
 
 def start(update, context):
     global tasks
     global started
+    global timer
     if (len(tasks) > 0):
-        for task in tasks:
-            task.timer.cancel()
         del tasks[:]
 
-    if (started):
-        update.message.reply_text("The bot is already started. reloading.")
-    else:
-        update.message.reply_text("The bot is starting!")
-
     chat_id = update.message.chat_id
-    started = True
+    
+    # Get the way the system should import the tasks
     mode = ""
     if (len(context.args) == 0):
         mode = "file"
     else:
         # FIXME: check the arg
         mode = context.args[0]
-
     if (mode == "file"):
         if (len(context.args) == 2):
             startTasksFromFile(chat_id, context.args[1])
         else:
             startTasksFromFile(chat_id)
-
     else:
-        update.message.send_text("Invalid argument. Use /help for syntax")
+        update.message.send_text("Invalid argument. Use /help for syntax.")
+
+    if (started):
+        update.message.reply_text("The bot was already started. Reloaded.")
+    else:
+        update.message.reply_text("The bot is starting!")
+        #actually start the timer for checking active tasks
+        timer.start()
+        started = True
+
 
 
 def status(update, context):
@@ -154,9 +170,17 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+# Global variables
+tasks = []
+bot = Bot(getToken())
+updater = Updater(bot=bot, use_context=True)
+started = False
+timer = perpetualTimer(updateSpeed, handleActiveTasks)
+
 # Main
 def main():
     dp = updater.dispatcher
+    dp.add_handler(CommandHandler("debug", debug))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("welcome", welcome))
     dp.add_handler(CommandHandler("status", status))
